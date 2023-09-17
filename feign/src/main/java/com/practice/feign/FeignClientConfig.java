f830ee266dc8cce3a2edc694dff6a2c27f17b8ed
gyucheol0 <gyucheol.kim@yanolja.com>
2023-09-18 00:40:12 +0900

    timeout set

--- feign/src/main/java/com/practice/feign/FeignClientConfig.java
+++ feign/src/main/java/com/practice/feign/FeignClientConfig.java
@@ -1,9 +1,16 @@
 package com.practice.feign;
 
+import feign.Logger;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients
 public class FeignClientConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
