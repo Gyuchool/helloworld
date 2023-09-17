79f0c1636def9fa86e0c0dab5ba55666adcbca7d
gyucheol0 <gyucheol.kim@yanolja.com>
2023-09-17 22:59:32 +0900

    add comment

--- feign/src/main/java/com/practice/feign/UserClient.java
+++ feign/src/main/java/com/practice/feign/UserClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "userClient", url = "https://randomuser.me")
 public interface UserClient {
 
+    // https://randomuser.me/api?nat={nation} 형태로 호출
     @GetMapping("/api")
     UserResponse getUser(@RequestParam("nat") String nation);
 }
