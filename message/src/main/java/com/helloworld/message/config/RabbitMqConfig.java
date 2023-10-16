20c0d7721575f21035f7b1bb39a9d18ec23b0f4f
Gyuchool <balance0591@naver.com>
2023-10-16 22:05:18 +0900

    deadletter queue config

--- message/src/main/java/com/helloworld/message/config/RabbitMqConfig.java
+++ message/src/main/java/com/helloworld/message/config/RabbitMqConfig.java
@@ -1,15 +1,25 @@
 package com.helloworld.message.config;
 
-import com.rabbitmq.client.ConnectionFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.helloworld.message.MessageDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class RabbitMqConfig {
 
@@ -31,13 +41,32 @@ public class RabbitMqConfig {
     @Value("${spring.rabbitmq.routing.key}")
     private String routingKey;
 
+    private final ObjectMapper objectMapper;
+
     @Bean
     public Queue queue() {
-
         return QueueBuilder
                 .durable(queueName)
+                .deadLetterExchange(exchangeName+".dlx")
                 .ttl(300_000)
-                .build(); //...false
+                .build();
+    }
+
+    @Bean
+    Queue deadLetterQueue() {
+        return QueueBuilder.durable("giron.deadletter")
+                .build();
+    }
+
+    @Bean
+    FanoutExchange deadLetterExchange() {
+        return new FanoutExchange(exchangeName+".dlx");
+    }
+
+
+    @Bean
+    Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange());
     }
 
     @Bean
@@ -53,15 +82,6 @@ public class RabbitMqConfig {
     }
 
     @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setUsername(rabbitmqUsername);
-        connectionFactory.setPassword(rabbitmqPassword);
-        connectionFactory.setVirtualHost(rabbitmqVhost);
-        return connectionFactory.getRabbitConnectionFactory();
-    }
-
-    @Bean
     public RabbitTemplate rabbitTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setConfirmCallback(((correlationData, ack, cause) -> {
@@ -74,4 +94,37 @@ public class RabbitMqConfig {
         return rabbitTemplate;
     }
 
+    @Component
+    public class DeadLetterMessageListener implements MessageListener {
+
+        @Override
+        public void onMessage(Message message) {
+            try {
+                MessageDto messageDto = objectMapper.readValue(message.getBody(), MessageDto.class);
+                log.info("Received message from DLQ: {}", messageDto);
+
+            }catch(Exception e){
+                log.error("Error while processing message from DLQ", e);
+                //reject the message
+            }
+        }
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             DeadLetterMessageListener listener) {
+        SimpleRabbitListenerContainerFactory containerFactory = new SimpleRabbitListenerContainerFactory();
+        containerFactory.setAdviceChain(
+                RetryInterceptorBuilder.stateless()
+                        .maxAttempts(3)
+                        .backOffOptions(1000, 2, 2000)
+                        .recoverer(new RejectAndDontRequeueRecoverer())
+                        .build()
+        );
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueues(deadLetterQueue());
+        container.setMessageListener(listener);
+        return container;
+    }
 }
--- message/src/main/java/com/helloworld/message/consumer/MessageConsumer.java
+++ message/src/main/java/com/helloworld/message/consumer/MessageConsumer.java
@@ -1,10 +1,13 @@
 package com.helloworld.message.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.helloworld.message.MessageDto;
 import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,10 +17,27 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class MessageConsumer {
 
+    @Value("${spring.rabbitmq.queue.name}")
+    private String queueName;
+
+    private final ObjectMapper objectMapper;
+
     @RabbitListener(queues = "${spring.rabbitmq.queue.name}")
-    public void receiveMessage(Message message, Channel channel) {
+    public void receiveMessage(Message message, Channel channel) throws IOException {
         long tag = message.getMessageProperties().getDeliveryTag();
         log.info("Received message:{}, Delivery tag:{}", new String(message.getBody()), tag);
+
+        MessageDto dto;
+        try {
+            dto = objectMapper.readValue(message.getBody(), MessageDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (dto.getId() %2 == 0) {
+            log.info("Even number!! go to dead letter queue:"+dto);
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+//            throw new AmqpRejectAndDontRequeueException("Even number!! go to dead letter queue:"+dto);
+        }
 
         try {
             // 처리가 성공적으로 완료되면 메시지에 대한 ACK 전송
@@ -25,9 +45,9 @@ public class MessageConsumer {
         } catch (Exception e) {
             // 처리 중 예외가 발생한 경우 메시지에 대한 NACK 전송
             try {
-                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
             } catch (IOException ex) {
-                log.error("nack error:{}", ex.getMessage(), ex);
+                log.info("nack error:{}", ex.getMessage(), ex);
                 throw new RuntimeException(ex);
             }
         }
